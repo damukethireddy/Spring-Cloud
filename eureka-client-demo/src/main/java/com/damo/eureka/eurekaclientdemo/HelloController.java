3770c13b8dc05f00bcbcb70407bb7cb2f892f7ce
damukethireddy <38098491+damukethireddy@users.noreply.github.com>
2020-07-08 18:16:44 +0530

    Added RefreshScope

--- eureka-client-demo/src/main/java/com/damo/eureka/eurekaclientdemo/HelloController.java
+++ eureka-client-demo/src/main/java/com/damo/eureka/eurekaclientdemo/HelloController.java
@@ -2,7 +2,8 @@ package com.damo.eureka.eurekaclientdemo;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+//import org.springframework.cloud.context.config.annotation.RefreshScope;
+//@RefreshScope
 @RestController
 public class HelloController {
 
